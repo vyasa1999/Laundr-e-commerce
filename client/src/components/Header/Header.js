109a27d68eb791ff40cada357ae2a9a14de26a02
Paulina Liwanag <liwanag.paulina@gmail.com>
2020-10-14 17:00:50 -0400

    add mdi, bootstrap cdn, semantic ui cdn; edit header, utils

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import laundrLogo from "../../assets/laundr-assets/laundr-logo.png";
+import Icon from "@mdi/react"
+import {mdiCartOutline} from "@mdi/js"
 
 const Header = () => {
     return (
-        <div className='topnav'>
-            {/* Logo */}
-            <Link id="logo-link" to="/">
-                <img className="topnav-logo" src={ "/logo192.png" } alt="React logo" />
-            </Link>
+        <div className="navbar navbar-expand-lg navbar-light header">
+            <div className='topnav '>
+                {/* Logo */}
+                <Link id="logo-link" to="/">
+                    <img className="topnav-logo" src={ laundrLogo } alt="Laundr logo" />
+                </Link>
 
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            </div>
+            {/* from template */}
             {/* Page Links */}
-            <div className="topnav-right">
-                <Link className="topnav-link" to='/Register'>Sign in</Link>
-                <Link className="topnav-link" to='/projects'>Projects</Link>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-                    Events
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
-                </a>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-                    Resources
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
-                </a>
-                <Link className="topnav-link" to="/about">About</Link>
+            <div className="topnav-right collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="topnav-link nav-link" to='/'>Home </Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="topnav-link nav-link" to='/about'>Our Story</Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="topnav-link nav-link" to="/"><Icon path={mdiCartOutline} title="Shopping Cart" size={2} color="white"/></Link>
+                    </li>
+                </ul>
+                {/* <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
+                    <i class="fas fa-shopping-cart"></i>
+                </a> */}
             </div>
         </div>
     )
