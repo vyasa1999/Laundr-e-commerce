105e2c4cc687f37d18789a4975bc64a5de64272d
Jack Byrnes <jackbyrnes1998@yahoo.com>
2020-11-09 15:11:43 -0500

    Fixed footer sliding

--- client/src/views/ProductOnScroll/ProductOnScroll.js
+++ client/src/views/ProductOnScroll/ProductOnScroll.js
@@ -10,44 +10,51 @@ import Mahogany from "./Product5Mahogany/Mahogany";
 import Vanilla from "./Product4Vanilla/Vanilla";
 import Watermelon from "./Product6Watermelon/Watermelon";
 import HomeFooter from "../HomeFooter/HomeFooter";
+import ReactFullpage from '@fullpage/react-fullpage';
 
+const ProductOnScroll = () => (
+    <ReactFullpage
+        //fullpage options
+        scrollingSpeed = {1000} /* Options here */
 
+        render={({ state, fullpageApi }) => {
+            return (
+                <ReactFullpage.Wrapper>
+                    <div className="section" id="section0">
+                        <Home />
+                    </div>
 
-export default class ProductOnScroll extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { currentPage: null };
-    }
+                    <div className="section" id="section1">
+                        <FreshAir />
+                    </div>
 
-    handlePageChange = number => {
-        this.setState({ currentPage: number });
-    };
+                    <div className="section" id="section2">
+                        <Gardenia />
+                    </div>
 
-    handleBeforePageChange = number => {
-        console.log(number);
-    };
+                    <div className="section" id="section3">
+                        <Eucalyptus />
+                    </div>
 
+                    <div className="section" id="section4">
+                        <Mahogany />
+                    </div>
 
-    render() {
-        return (
-            <React.Fragment>
-                <ReactPageScroller
-                    pageOnChange={this.handlePageChange}
-                    onBeforePageScroll={this.handleBeforePageChange}
-                    customPageNumber={this.state.currentPage}
-                    >
-                    <Home />
-                    <FreshAir />
-                    <Gardenia />
-                    <Eucalyptus />
-                    <Mahogany />
-                    <Vanilla />
-                    <Watermelon />
+                    <div className="section" id="section5">
+                        <Vanilla  />
+                    </div>
 
-                </ReactPageScroller>
-                <HomeFooter />
-            </React.Fragment>
+                    <div className="section" id="section6">
+                        <Watermelon  />
+                    </div>
 
-        );
-    }
-}
+                    <div className="section fp-auto-height" id="section7">
+                        <div id="footer"> <HomeFooter /> </div>
+                    </div>
+
+                </ReactFullpage.Wrapper>
+            );
+        }}
+    />
+);
+export default ProductOnScroll
