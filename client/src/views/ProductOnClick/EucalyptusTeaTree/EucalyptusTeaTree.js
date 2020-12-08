8f22427c45a9611c5bb5edd0061b37cccec362e2
Jack Byrnes <jackbyrnes1998@yahoo.com>
2020-12-07 21:41:06 -0500

    productonClick animation crappy implementation

--- client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
+++ client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import "./EucalyptusTeaTree.scss"
-
-$(window).on("load resize scroll", function () {
-    $(".bg-static").each(function () {
-        var windowTop = $(window).scrollTop();
-        var elementTop = $(this).offset().top;
-        var leftPosition = windowTop - elementTop;
-        $(this)
-            .find(".bg-move")
-            .css({left: leftPosition});
-    });
-});
+import "../../../functions/ParallaxEffect"
 
 const EucalyptusTeaTree = () => {
     return (
