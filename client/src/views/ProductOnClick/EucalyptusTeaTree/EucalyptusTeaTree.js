1ceab141ced2943f609a4bdc23c65302029598dc
Jack Byrnes <jackbyrnes1998@yahoo.com>
2020-12-07 21:39:33 -0500

    productonClick animation crappy implementation

--- client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
+++ client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
@@ -1,21 +1,59 @@
 import React from 'react';
-import "../../Product/Product.scss"
+import $ from 'jquery';
+import "./EucalyptusTeaTree.scss"
+
+$(window).on("load resize scroll", function () {
+    $(".bg-static").each(function () {
+        var windowTop = $(window).scrollTop();
+        var elementTop = $(this).offset().top;
+        var leftPosition = windowTop - elementTop;
+        $(this)
+            .find(".bg-move")
+            .css({left: leftPosition});
+    });
+});
 
 const EucalyptusTeaTree = () => {
     return (
         <div className="eucalyptus">
-            <div className="row">
-                <div className="col">
-                    <div className="product">
-                        <div className="mockup"></div>
+            <head>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            </head>
+
+            <div className="sectionEucalyptus bg-static">
+                <div className="bg-move"></div>
+            </div>
+
+            <div className="sectionEucalyptus" id="p1">
+                <div className="row">
+                    <div className="col">
+                        <div className="product">
+                            <div className="mockup"></div>
+                        </div>
+                    </div>
+
+                    <div className="col productInfo">
+                        <h1 className='display-2'>Eucalyptus Tea Tree</h1>
+                        <h2>$18.99</h2>
+                        <p>A natural scent inspired by popular aromatherapy oil mixtures. Great for towels, bath robes, and
+                            having a “me day.”
+                        </p>
+                        <a href="/products/#LPETT">Back</a>
+                    </div>
+                    <div className="col productInfo">
+                        <h1>
+                            This fragrance has hints of:
+                            <ul>
+                                <li>Tea Tree and Eucalyptus</li>
+                                <li>Natural Herbs</li>
+                                <li>Bergamot Orange</li>
+                            </ul>
+                        </h1>
                     </div>
                 </div>
-                <div className="col productInfo">
-                    <h1 className='display-2'>Eucalyptus Tea Tree</h1>
-                    <h2>$18.99</h2>
-                    <a href="/products/#LPETT">Back</a>
-                </div>
             </div>
+
+
         </div>
     )
 }
