977336da8157cd4c4ead7ea9c360b4fb1992bee7
Jack Byrnes <jackbyrnes1998@yahoo.com>
2020-12-09 04:20:47 -0500

    implement all product on click pages, clean up layout

--- client/src/views/Checkout/Checkout.js
+++ client/src/views/Checkout/Checkout.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Checkout.scss';
 
 import CreditCardForm from "../../components/CreditCardForm/CreditCardForm"
+import CheckoutForm from "../../components/CreditCardForm/testcreditcard";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const promise = loadStripe("pk_test_51HwGK7GsBd8lMjUXZuKZ3xfG2GmeFDGa96s0IXrHG2MsIXg3kdeo75dPfCr4SWtn6tSRWQffLm0mSa0sxrhVaABZ00yeuD08Rl");
 
 function Checkout() {
     return (
         <div className="Checkout">
             <div className="container">
-                <CreditCardForm />
+                <Elements stripe={promise}>
+                    <CheckoutForm />
+                </Elements>
             </div>
         </div>
     );
--- client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
+++ client/src/views/ProductOnClick/EucalyptusTeaTree/EucalyptusTeaTree.js
@@ -1,9 +1,66 @@
-import React from 'react';
-import $ from 'jquery';
+import React, {useState} from 'react';
 import "./EucalyptusTeaTree.scss"
 import "../../../functions/ParallaxEffect"
 
-const EucalyptusTeaTree = () => {
+const EucalyptusTeaTree = (props) => {
+    const [productToAdd, setProductToAdd] = useState([{
+        name: props.productName,
+        price: 18.99,
+        productID: props.productID
+    }])
+
+    const [isCartEmpty, setIsCartEmpty] = useState(true)
+    const [cartSize, setCartSize] = useState(1)
+
+    const addToCart = () => {
+        setIsCartEmpty(false)
+
+        try {
+            const res = fetch("http://localhost:5000/api/cart/" + props.productID + "/" + "1", {
+                method: "POST",
+                body: JSON.stringify({
+                    productId: props.productID,
+                    quantity: 1,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            });
+            console.log(res);
+            // fetchCart();
+            // alert("Item Incremented");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const increaseQty = () => {
+        setCartSize(cartSize + 1)
+
+        try {
+            const res = fetch("http://localhost:5000/:productId/:quantity", {
+                method: "POST",
+                body: JSON.stringify({
+                    productId: props.productID,
+                    quantity: 1,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            });
+            console.log(res);
+            // fetchCart();
+            // alert("Item Incremented");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const decreaseQty = () => {
+        setCartSize(cartSize - 1)
+        if(cartSize === 0) {
+            setIsCartEmpty(true)
+        }
+    }
+
     return (
         <div className="eucalyptus">
             <head>
@@ -26,19 +83,27 @@ const EucalyptusTeaTree = () => {
                         <h1 className='display-2'>Eucalyptus Tea Tree</h1>
                         <h2>$18.99</h2>
                         <p>A natural scent inspired by popular aromatherapy oil mixtures. Great for towels, bath robes, and
-                            having a “me day.”
-                        </p>
-                        <a href="/products/#LPETT">Back</a>
-                    </div>
-                    <div className="col productInfo">
-                        <h1>
-                            This fragrance has hints of:
-                            <ul>
-                                <li>Tea Tree and Eucalyptus</li>
-                                <li>Natural Herbs</li>
-                                <li>Bergamot Orange</li>
-                            </ul>
-                        </h1>
+                            having a “me day.”</p>
+
+                        <br />
+                        This fragrance has hints of:
+                        <ul>
+                            <li>Tea Tree and Eucalyptus</li>
+                            <li>Natural Herbs</li>
+                            <li>Bergamot Orange</li>
+                        </ul>
+
+                        {isCartEmpty
+                            ? <button onClick={addToCart} className="btn btn-md btn-info">Add to Cart</button>
+                            : <div>
+                                <button onClick={decreaseQty} className="btn btn-danger">-</button>
+                                {cartSize}
+                                <button onClick={increaseQty} className="btn btn-danger">+</button>
+                            </div>
+                        }
+                        <button className="btn btn-light">
+                            <a href="/products/#LPETT">Back</a>
+                        </button>
                     </div>
                 </div>
             </div>
