9c4a8ec914ab383d49380d028f0c69c04036b489
Paulina Liwanag <liwanag.paulina@gmail.com>
2020-12-09 18:06:59 -0500

    attempting heroku deploy

---
+++ client/build/precache-manifest.97686c5549e58ce37a2cd6a71096c683.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c61d244e05023d26a5a494ca69d93a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe0ffc4f44f8520cd54",
+    "url": "/static/css/main.71e29b30.chunk.css"
+  },
+  {
+    "revision": "1b6ef8b6f5e84e1a3b41",
+    "url": "/static/js/2.46013e54.chunk.js"
+  },
+  {
+    "revision": "8fe0ffc4f44f8520cd54",
+    "url": "/static/js/main.6b7db8f4.chunk.js"
+  },
+  {
+    "revision": "1979d029c18bafb1d688",
+    "url": "/static/js/runtime-main.e2a4b577.js"
+  },
+  {
+    "revision": "d773e696b94f47cb17172445a660072d",
+    "url": "/static/media/Laundr Bomb Angle1.d773e696.png"
+  },
+  {
+    "revision": "075c9932abf95b59a1aead961e7d3325",
+    "url": "/static/media/coffeeMockup.075c9932.png"
+  },
+  {
+    "revision": "e20c872ad379d6a37ab7638cfe9841cf",
+    "url": "/static/media/eucalyptusMockup.e20c872a.png"
+  },
+  {
+    "revision": "ddc7d57f02df04b67670788f546cdb75",
+    "url": "/static/media/eucalyptus_pattern.ddc7d57f.png"
+  },
+  {
+    "revision": "4bdc9b2a72ea1a2109ef8e1ef8935182",
+    "url": "/static/media/ezgif.com-gif-maker.4bdc9b2a.gif"
+  },
+  {
+    "revision": "99c5add97c1d87abd0614fd7aa9ff5c1",
+    "url": "/static/media/freshairMockup.99c5add9.png"
+  },
+  {
+    "revision": "adda6d5d2cd492fe545f7a11c9b4defd",
+    "url": "/static/media/freshair_pattern.adda6d5d.png"
+  },
+  {
+    "revision": "2f5c25912c88ca64cd07c84631a75690",
+    "url": "/static/media/gardeniaMockup.2f5c2591.png"
+  },
+  {
+    "revision": "1718f9851228f07ed1e2432773f61c24",
+    "url": "/static/media/gardenia_pattern.1718f985.png"
+  },
+  {
+    "revision": "1d7366fb98fae4fd4ee4b2f3182540a4",
+    "url": "/static/media/mahogany_pattern.1d7366fb.png"
+  },
+  {
+    "revision": "bbded255f9b2afd70c439e615aa2f3d0",
+    "url": "/static/media/mahoganymockup.bbded255.png"
+  },
+  {
+    "revision": "69543170f1be183ee5e864d23b686499",
+    "url": "/static/media/vanilla_pattern.69543170.png"
+  },
+  {
+    "revision": "ac039e9ad6af5b9104306f0bc2353b3d",
+    "url": "/static/media/watermelonMockup.ac039e9a.png"
+  },
+  {
+    "revision": "7bf196176a61898c2f2454458a3d6667",
+    "url": "/static/media/watermelon_pattern.7bf19617.png"
+  }
+]);
